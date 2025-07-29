28ee28a4eb06dece6cba6812db28d244e2a94c61
Bulat Zaripov <zaripov291096@gmail.com>
2025-07-29 11:09:25 +0300

    feat: add Modules/TodoList module

---
+++ EffectiveTodoApp/Modules/TodoList/Interactor/TodoListInteractor.swift
@@ -0,0 +1,45 @@
+//
+//  TodoListInteractor.swift
+//  EffectiveToDoApp
+//
+//  Created by Bulat Zaripov on 26.07.2025.
+//
+
+import Foundation
+
+final class TodoListInteractor {
+    weak var presenter: TodoListInteractorOutputProtocol?
+    var storageService: TodosStorageServiceProtocol!
+}
+
+// MARK: - TodoListInteractorProtocol
+
+extension TodoListInteractor: TodoListInteractorProtocol {
+    func createTodo() {
+        storageService.create { [weak self] todo in
+            DispatchQueue.main.async {
+                self?.presenter?.didCreateTodo(todo)
+            }
+        }
+    }
+    
+    func fetchTodos() {
+        storageService.fetchTodos { [weak self] todos in
+            DispatchQueue.main.async {
+                self?.presenter?.didFetchTodos(todos)
+            }
+        }
+    }
+
+    func searchTodos(with query: String) {
+        storageService.searchTodos(by: query) { [weak self] todos in
+            DispatchQueue.main.async {
+                self?.presenter?.didFetchTodos(todos)
+            }
+        }
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        storageService.delete(todo: todo, completion: nil)
+    }
+}
---
+++ EffectiveTodoApp/Modules/TodoList/Presenter/TodoListPresenter.swift
@@ -0,0 +1,79 @@
+//
+//  TodoListPresenter.swift
+//  EffectiveToDoApp
+//
+//  Created by Bulat Zaripov on 26.07.2025.
+//
+
+import Foundation
+
+final class TodoListPresenter {
+    
+    // MARK: - Dependencies
+    
+    weak var view: TodoListViewProtocol?
+    var interactor: TodoListInteractorProtocol!
+    var router: TodoListRouterProtocol!
+    
+    // MARK: - State
+    
+    var todos: [Todo] = []
+}
+
+// MARK: - TodoListPresenterProtocol
+
+extension TodoListPresenter: TodoListPresenterProtocol {
+    func viewDidLoad() {
+        interactor.fetchTodos()
+    }
+    
+    func viewWillAppear() {
+        interactor.fetchTodos()
+        view?.updateTodoCount(todos.count)
+    }
+    
+    func didSearchTextChange(_ text: String) {
+        if text.isEmpty {
+            view?.displayTodos(todos)
+            view?.updateTodoCount(todos.count)
+        } else {
+            interactor.searchTodos(with: text)
+        }
+    }
+    
+    func didTapAddButton() {
+        interactor.createTodo()
+    }
+    
+    func didSelectTodo(_ todo: Todo) {
+        router.openTodoDetail(from: view!, todo: todo)
+    }
+    
+    func didDeleteTodo(_ todo: Todo) {
+        interactor.deleteTodo(todo)
+        if let index = todos.firstIndex(of: todo) {
+            todos.remove(at: index)
+            view?.removeTodo(with: index)
+            view?.updateTodoCount(todos.count)
+        }
+    }
+}
+
+// MARK: - TodoListInteractorOutputProtocol
+
+extension TodoListPresenter: TodoListInteractorOutputProtocol {
+    func didFetchTodos(_ todos: [Todo]) {
+        self.todos = todos
+        view?.displayTodos(todos)
+        view?.updateTodoCount(todos.count)
+    }
+    
+    func didSearchTodos(_ todos: [Todo]) {
+        view?.displayTodos(todos)
+        view?.updateTodoCount(todos.count)
+    }
+    
+    func didCreateTodo(_ todo: Todo) {
+        router.openTodoDetail(from: view!, todo: todo)
+    }
+}
